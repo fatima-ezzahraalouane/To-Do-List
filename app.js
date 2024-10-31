0a638e1b7de5cf4b32c41b6bed9748332975c316
fatima-ezzahraalouane <falouane38@gmail.com>
2024-10-31 17:08:39 +0100

    En tant que développeuse Front-End, j'ai créé un modal permettant d'ajouter une nouvelle tâche

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const addTaskButton = document.getElementById('addTaskButton');
-    const taskModal = document.getElementById('modal');
-    const cancelButton = document.getElementById('cancelButton');
-    const addTask = document.getElementById('addTask');
+    const addTaskButton = document.getElementById("addTaskButton");
+    const taskModal = new bootstrap.Modal(document.getElementById("taskModal"));
+    const taskForm = document.getElementById("taskForm");
+    // const totalTaks = document.getElementById("totalTasks");
 
-    addTaskButton.addEventListener('click', () => {
-        modal.style.display = 'flex';
-    });
+    // const cancelButton = document.getElementById('cancelButton');
+    // const addTask = document.getElementById('addTask');
 
-    cancelButton.addEventListener('click', () => {
-        modal.style.display = 'none';
-    });
+    // addTaskButton.addEventListener('click', () => {
+    //     modal.style.display = 'flex';
+    // });
 
-    addTask.addEventListener('click', () => {
-        const title = document.getElementById('taskTitle').value;
-        const description = document.getElementById('taskDescription').value;
-        const status = document
-    });
+    // cancelButton.addEventListener('click', () => {
+    //     modal.style.display = 'none';
+    // });
+    
+    let tasks = [];
+
+    addTaskButton.onclick = () => taskModal.show();
+
+    taskForm.onsubmit = (e) => {
+        e.preventDefault();
+        const title = document.getElementById("taskTitle").value;
+        const description = document.getElementById("taskDescription").value;
+        const status = document.getElementById("taskStatus").value;
+        const dueDate = document.getElementById("taskDueDate").value;
+        const priority = document.getElementById("taskPriority").value;
+
+        const task = { id: Date.now(), title, description, status, dueDate, priority };
+        tasks.push(task);
+        updateTaskLists();
+        taskForm.reset();
+        taskModal.hide();
+
+    };
+
+    function updateTaskLists() {
+
+    }
+
+
+
+    // addTask.addEventListener('click', () => {
+    //     const title = document.getElementById('taskTitle').value;
+    //     const description = document.getElementById('taskDescription').value;
+    //     const status = document.getElementById('taskStatus').value;
+    //     const deadline = document.getElementById('taskDeadline').value;
+    //     const priority = document.getElementById('taskPriority').value;
+
+    // });
 });
